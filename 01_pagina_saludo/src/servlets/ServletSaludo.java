dd3035eff6662e5f1b1e14f9e8f579de0be698b5
ALUMNO <ALUMNO@Aula-Pc>
2019-06-25 10:15:41 +0200

    add:"... final de mi casa ♥"

--- 01_pagina_saludo/src/servlets/ServletSaludo.java
+++ 01_pagina_saludo/src/servlets/ServletSaludo.java
@@ -26,7 +26,7 @@ public class ServletSaludo extends HttpServlet {
 		out.println("<body><center>");
 
 		for (int i = 1; i <= 6; i++) {
-			out.println("<h" + i + ">Bienvenido a mi servlet </h" + i + ">");
+			out.println("<h" + i + ">Bienvenido a mi servlet final de mi casa♥</h" + i + ">");
 		}
 
 		out.println("</center></body>");
